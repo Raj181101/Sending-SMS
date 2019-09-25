13992af686102cceeefc78f12d185a4eaf4ddf90
Raj181101 <rajasekharderangula489@gmail.com>
2019-09-25 12:28:27 +0530

    first commit

---
+++ SMS.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+URL = 'https://www.way2sms.com/api/v1/sendCampaign'
+
+# get request
+def sendPostRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textMessage):
+  req_params = {
+  'apikey':apiKey,
+  'secret':secretKey,
+  'usetype':useType,
+  'phone': phoneNo,
+  'message':textMessage,
+  'senderid':senderId
+  }
+  return requests.post(reqUrl, req_params)
+
+# get response
+response = sendPostRequest(URL, '8YGQS8946AB1KP8U2EWRHS7XC9XQHKIS', '06454GN4ROIA6QXH', 'stage', '8074060937', '', 'Welcome Praveen Nice To Meet u' )
+"""
+  Note:-
+    you must provide apikey, secretkey, usetype, mobile, senderid and message values
+    and then requst to api
+"""
+# print response if you want
+print(response.text)
